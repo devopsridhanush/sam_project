cf420d4ffadcdb2a0348bd591ab67f40c433137c
AmarNaidu <amarnaidu.akula@gmail.com>
2025-05-26 15:20:26 +0530

    sample Commit

--- functions/charge_payment/app.py
+++ functions/charge_payment/app.py
@@ -3,8 +3,6 @@ import time
 def lambda_handler(event, context):
     print("Processing payment for order:", event.get("order_id"))
 
-    # Test for modifying the lambda function code
-
     # Simulate payment processing delay
     time.sleep(2)
 
